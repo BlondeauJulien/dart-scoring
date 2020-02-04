ba9608d5708bdba75dd3830b946acd0a110e6495
Julien Blondeau <julienblondeau@live.fr>
2020-02-04 16:47:43 +0100

    Updates

---
+++ precache-manifest.5a8fe1e8aba9e61abf506ad4cd3b1c2b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24a65a84f9faea8338146715ed38cd8",
+    "url": "/dart-scoring/index.html"
+  },
+  {
+    "revision": "ae95ab3c98cfc8f0e759",
+    "url": "/dart-scoring/static/css/main.138ed2e0.chunk.css"
+  },
+  {
+    "revision": "b878fcb82e2f6f57c703",
+    "url": "/dart-scoring/static/js/2.cbe3276e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/dart-scoring/static/js/2.cbe3276e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae95ab3c98cfc8f0e759",
+    "url": "/dart-scoring/static/js/main.92c43013.chunk.js"
+  },
+  {
+    "revision": "5a64bbb2afe29f7450dc",
+    "url": "/dart-scoring/static/js/runtime-main.4916d715.js"
+  },
+  {
+    "revision": "41b0b1f46007a816c37a2bdc0c398879",
+    "url": "/dart-scoring/static/media/dartboard.41b0b1f4.png"
+  },
+  {
+    "revision": "078aa3f09332c2484dbfd7239ef39e2d",
+    "url": "/dart-scoring/static/media/spinner.078aa3f0.gif"
+  }
+]);
